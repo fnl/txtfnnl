d0188554437b5f8cc938421ca122e69c4ae0b9b7
fnl <florian.leitner@gmail.com>
2012-06-26 19:05:26 +0200

    added tests for the SentenceExtractor

---
+++ txtfnnl-bin/src/test/java/txtfnnl/pipelines/SentenceExtractorTest.java
@@ -0,0 +1,105 @@
+package txtfnnl.pipelines;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.logging.Level;
+
+import org.junit.Test;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
+
+import org.uimafit.testing.util.DisableLogging;
+
+import txtfnnl.opennlp.uima.sentdetect.SentenceLineWriter;
+import txtfnnl.uima.collection.FileCollectionReader;
+import txtfnnl.uima.collection.FileSystemCollectionReader;
+
+public class SentenceExtractorTest {
+
+	public static File mkTmpDir() throws IOException {
+		File tmp = File
+		    .createTempFile("tmp", Long.toString(System.nanoTime()));
+
+		if (!tmp.delete())
+			throw new IOException("could not delete tmp file: " +
+			                      tmp.getAbsolutePath());
+
+		if (!tmp.mkdir())
+			throw new IOException("could not create tmp dir: " +
+			                      tmp.getAbsolutePath());
+
+		return tmp;
+	}
+
+	@Test
+	public void testDirectoryReaderSetup() throws IOException, UIMAException {
+		File inputDir = mkTmpDir();
+		SentenceExtractor se = new SentenceExtractor(inputDir, "mime type",
+		    true, null, null, true);
+		ConfigurationParameterSettings cps = se.collectionReader
+		    .getCollectionReaderMetaData().getConfigurationParameterSettings();
+
+		assertEquals(inputDir.getCanonicalPath(),
+		    cps.getParameterValue(FileSystemCollectionReader.PARAM_DIRECTORY));
+		assertEquals("mime type",
+		    cps.getParameterValue(FileSystemCollectionReader.PARAM_MIME_TYPE));
+		assertEquals(Boolean.TRUE,
+		    cps.getParameterValue(FileSystemCollectionReader.PARAM_RECURSIVE));
+	}
+
+	@Test
+	public void testFileReaderSetup() throws IOException, UIMAException {
+		File inputFile = File.createTempFile("out",
+		    Long.toString(System.nanoTime()));
+		String[] inputFiles = new String[] { inputFile.getAbsolutePath() };
+		SentenceExtractor se = new SentenceExtractor(inputFiles, null, null,
+		    null, true);
+		ConfigurationParameterSettings cps = se.collectionReader
+		    .getCollectionReaderMetaData().getConfigurationParameterSettings();
+
+		assertArrayEquals(inputFiles,
+		    (Object[]) cps.getParameterValue(FileCollectionReader.PARAM_FILES));
+		assertNull(cps.getParameterValue(FileCollectionReader.PARAM_MIME_TYPE));
+	}
+
+	@Test
+	public void testLineWriterSetup() throws IOException, UIMAException {
+		File inputFile = File.createTempFile("input",
+		    Long.toString(System.nanoTime()));
+		String[] inputFiles = new String[] { inputFile.getAbsolutePath() };
+		File outputDir = mkTmpDir();
+		SentenceExtractor se = new SentenceExtractor(inputFiles, null,
+		    outputDir, "encoding", true);
+		ConfigurationParameterSettings cps = se.sentenceLineWriter
+		    .getMetaData().getConfigurationParameterSettings();
+
+		assertEquals("encoding",
+		    cps.getParameterValue(SentenceLineWriter.PARAM_ENCODING));
+		assertEquals(outputDir.getCanonicalPath(),
+		    cps.getParameterValue(SentenceLineWriter.PARAM_OUTPUT_DIRECTORY));
+		assertEquals(Boolean.TRUE,
+		    cps.getParameterValue(SentenceLineWriter.PARAM_OVERWRITE_FILES));
+	}
+
+	@Test
+	public void testRunningThePipeline() throws UIMAException, IOException {
+		
+		File inputFile = new File("src/test/resources/pubmed.xml");
+		assert inputFile.exists() : "test file does not exist";
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		DisableLogging.enableLogging(Level.WARNING);
+		SentenceExtractor se = new SentenceExtractor(
+		    new String[] { inputFile.getCanonicalPath() }, null, null,
+		    "UTF-8", false);
+		se.run();
+		String content = outContent.toString();
+		assertTrue(content.indexOf("studied.\nAs(4)O(6)") > 0);
+		System.setOut(null);
+	}
+}
