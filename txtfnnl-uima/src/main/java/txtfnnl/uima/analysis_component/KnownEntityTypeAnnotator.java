0020103c8fce72fe074d50084a2c2d2340ff2688
fnl <florian.leitner@gmail.com>
2012-06-29 10:28:04 +0200

    renamed the DB resource key and added documentation

--- txtfnnl-uima/src/main/java/txtfnnl/uima/analysis_component/KnownEntityTypeAnnotator.java
+++ txtfnnl-uima/src/main/java/txtfnnl/uima/analysis_component/KnownEntityTypeAnnotator.java
@@ -28,6 +28,38 @@ import txtfnnl.uima.resource.EntityStringMapResource;
 import txtfnnl.uima.resource.JdbcConnectionResource;
 import txtfnnl.uima.tcas.SemanticAnnotation;
 
+/**
+ * 
+ * The <b>KnownEntities</b> resource has to be a TSV file with the following
+ * columns:
+ * <ol>
+ * <li>Document ID: SOFA URI basename without the file suffix</li>
+ * <li>Entity Type: will be the IDs of the SemanticAnnotations and the name
+ * type in the EntityNameDb</li>
+ * <li>Namespace: of the entity as used in the EntityNameDb</li>
+ * <li>Identifier: of the entity as used in the EntityNameDb</li>
+ * </ol>
+ * 
+ * The <b>EntityNameDb</b> resource has to be a database with the following
+ * schema:
+ * <ul>
+ * <li>Entity Type Name Tables: named after each Entity Type in KnownEntities
+ * <ol>
+ * <li><b>uid</b> - the UID for this entity and type</li>
+ * <li><b>name</b> - a name for this entity</li>
+ * </ol>
+ * Both uid and name together should be unique.</li>
+ * <li>Entity ID Tables: named after each Namespace in KnownEntities
+ * <ol>
+ * <li><b>id</b> - the ID for this entity in this namespace</li>
+ * <li><b>uid</b> - the UID for this entity and type</li>
+ * </ol>
+ * Only the id needs to be unique.</li>
+ * </ul>
+ * All DB columns should be declared "<i>not null</i>".
+ * 
+ * @author Florian Leitner
+ */
 public class KnownEntityTypeAnnotator extends JCasAnnotator_ImplBase {
 
 	public static final String PARAM_NAMESPACE = "Namespace";
@@ -53,7 +85,7 @@ public class KnownEntityTypeAnnotator extends JCasAnnotator_ImplBase {
 			documentEntityMap = (EntityStringMapResource) ctx
 			    .getResourceObject("KnownEntities");
 			connector = (JdbcConnectionResource) ctx
-			    .getResourceObject("EntityDb");
+			    .getResourceObject("EntityNameDb");
 		} catch (ResourceAccessException e) {
 			throw new ResourceInitializationException(e);
 		}
@@ -134,17 +166,17 @@ public class KnownEntityTypeAnnotator extends JCasAnnotator_ImplBase {
 	List<String> getNames(Entity entity) throws AnalysisEngineProcessException {
 		List<String> names = new LinkedList<String>();
 		Statement stmt;
-		
+
 		try {
 			stmt = conn.createStatement();
 			ResultSet result = stmt.executeQuery("SELECT name FROM ..."); // TODO
-			
+
 			while (result.next())
 				names.add(result.getString(1));
 		} catch (SQLException e) {
 			throw new AnalysisEngineProcessException(e);
 		}
-		
+
 		return names;
 	}
 }
