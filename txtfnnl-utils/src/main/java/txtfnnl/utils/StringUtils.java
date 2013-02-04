5f8b84ae1782592597d3e528305feb82c99a75f7
fnl <florian.leitner@gmail.com>
2013-02-04 20:09:19 +0100

    added String Iterator methods

--- txtfnnl-utils/src/main/java/txtfnnl/utils/StringUtils.java
+++ txtfnnl-utils/src/main/java/txtfnnl/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package txtfnnl.utils;
 
+import java.util.Iterator;
+
 /**
  * Utility functions for working with Strings.
  * 
@@ -17,12 +19,29 @@ public final class StringUtils {
     return b.toString();
   }
 
+  public static final String join(Iterator<String> stringIt) {
+    final StringBuffer b = new StringBuffer();
+    while (stringIt.hasNext())
+      b.append(stringIt.next());
+    return b.toString();
+  }
+
   public static final String join(String using, String... strings) {
     final StringBuffer b = new StringBuffer();
     final int last = strings.length - 1;
-    for (int i = 0; i <= last; i++) {
+    for (int i = 0; i < last; ++i) {
       b.append(strings[i]);
-      if (i != last) b.append(using);
+      b.append(using);
+    }
+    if (last != -1) b.append(strings[last]);
+    return b.toString();
+  }
+
+  public static final String join(String using, Iterator<String> stringIt) {
+    final StringBuffer b = new StringBuffer();
+    while (stringIt.hasNext()) {
+      b.append(stringIt.next());
+      if (stringIt.hasNext()) b.append(using);
     }
     return b.toString();
   }
@@ -30,10 +49,22 @@ public final class StringUtils {
   public static final String join(char using, String... strings) {
     final StringBuffer b = new StringBuffer();
     final int last = strings.length - 1;
-    for (int i = 0; i <= last; i++) {
+    for (int i = 0; i < last; ++i) {
       b.append(strings[i]);
-      if (i != last) b.append(using);
+      b.append(using);
     }
+    if (last != -1) b.append(strings[last]);
     return b.toString();
   }
+  
+  public static final String join(char using, Iterator<String> stringIt) {
+    final StringBuffer b = new StringBuffer();
+    while (stringIt.hasNext()) {
+      b.append(stringIt.next());
+      b.append(using);
+    }
+    return b.length() == 0 ? "" : b.replace(b.length() - 1, b.length(), "").toString();
+  }
+
+
 }
