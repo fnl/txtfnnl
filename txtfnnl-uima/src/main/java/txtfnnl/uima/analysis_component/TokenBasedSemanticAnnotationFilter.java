51400f428ec41bceb1bb49eab49cceadc7ffccc3
fnl <florian.leitner@gmail.com>
2013-05-14 20:39:07 +0200

    fixed cases with empty surrounding mappings

--- txtfnnl-uima/src/main/java/txtfnnl/uima/analysis_component/TokenBasedSemanticAnnotationFilter.java
+++ txtfnnl-uima/src/main/java/txtfnnl/uima/analysis_component/TokenBasedSemanticAnnotationFilter.java
@@ -276,7 +276,7 @@ public class TokenBasedSemanticAnnotationFilter extends JCasAnnotator_ImplBase {
         Map<SemanticAnnotation, Collection<TokenAnnotation>> tokensContainingSemAnns) {
       Collection<TokenAnnotation> tokens = tokensCoveredBySemAnn.get(ann);
       if (tokens == null) tokens = tokensContainingSemAnns.get(ann);
-      if (tokens == null) {
+      if (tokens == null || tokens.size() == 0) {
         before = null;
         current = null;
         first = null;
