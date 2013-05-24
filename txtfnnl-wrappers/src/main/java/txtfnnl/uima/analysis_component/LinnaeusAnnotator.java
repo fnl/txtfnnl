cb32b5614eee5c4d2cc3972c87f2acdaa43953f3
fnl <florian.leitner@gmail.com>
2013-05-24 18:41:41 +0200

    namespace configuration, docu, and cleanup

--- txtfnnl-wrappers/src/main/java/txtfnnl/uima/analysis_component/LinnaeusAnnotator.java
+++ txtfnnl-wrappers/src/main/java/txtfnnl/uima/analysis_component/LinnaeusAnnotator.java
@@ -25,14 +25,34 @@ import uk.ac.man.entitytagger.EntityTagger;
 import uk.ac.man.entitytagger.Mention;
 import uk.ac.man.entitytagger.matching.Matcher;
 
+/**
+ * A wrapper for Linnaeus requiring a Linnaeus properties file for configuration. As no settings
+ * are preconfigured, the wrapper can be used to run Linnaeus with any dictionary. The only other
+ * required configuration is the namespace to use on the semantic annotations that will be made. As
+ * Linnaeus is mainly made for species annotations, a taxonomy namespace is preconfigured as the
+ * default namespace.
+ * 
+ * @author Florian Leitner
+ */
 public class LinnaeusAnnotator extends JCasAnnotator_ImplBase {
   public static final String URI = LinnaeusAnnotator.class.getName();
-  public static final String NAMESPACE = "http://www.uniprot.org/taxonomy/"; // TODO
+  /** The default namespace used by this annotator if no other is given. */
+  public static final String DEFAULT_NAMESPACE = "http://www.uniprot.org/taxonomy/";
+  public static final String PARAM_ANNOTATION_NAMESPACE = "AnnotationNamespace";
+  @ConfigurationParameter(name = PARAM_ANNOTATION_NAMESPACE,
+      description = "The namespace to use for the annotations.",
+      defaultValue = DEFAULT_NAMESPACE)
+  private String namespace;
   public static final String PARAM_CONFIG_FILE_PATH = "ConfigFilePath";
   @ConfigurationParameter(name = PARAM_CONFIG_FILE_PATH,
       description = "A property file path with Linnaeus configuration data.")
   private String configFilePath;
-  /** A mapping of some Linnaeus ID to another. */
+  /**
+   * A mapping of the detected IDs to some other ID.
+   * <p>
+   * This can be used to change IDs in cases where necessary, e.g., to map strain IDs back to their
+   * species IDs.
+   */
   public static final String MODEL_KEY_ID_MAPPING_RESOURCE = "IdMappingResource";
   @ExternalResource(key = MODEL_KEY_ID_MAPPING_RESOURCE, mandatory = false)
   private LineBasedStringMapResource<String> idMapping;
@@ -45,12 +65,20 @@ public class LinnaeusAnnotator extends JCasAnnotator_ImplBase {
       setConfigurationFilePath(configFile);
     }
 
+    /** Create a new builder with a particular properties file. */
     public Builder(File configFile) {
       this(LinnaeusAnnotator.class, configFile);
     }
 
+    /** Set a different properties file. */
     public Builder setConfigurationFilePath(File configFile) {
       setRequiredParameter(PARAM_CONFIG_FILE_PATH, configFile.getAbsolutePath());
+      return this;
+    }
+
+    /** Set the namespace to use for the semantic annotations. */
+    public Builder setAnnotationNamespace(String namespace) {
+      setRequiredParameter(PARAM_ANNOTATION_NAMESPACE, namespace);
       return this;
     }
 
@@ -58,7 +86,7 @@ public class LinnaeusAnnotator extends JCasAnnotator_ImplBase {
      * Supply a {@link LineBasedStringMapResource} that maps Linnaeus IDs to another.
      * <p>
      * If set, all matching keys for an ID reported by Linnaeus will instead be annotated with the
-     * mapped target ID.
+     * mapped target ID. Particularly useful to map IDs such as strain IDs to their species IDs.
      */
     public Builder setIdMappingResource(ExternalResourceDescription idMappingResource) {
       setOptionalParameter(MODEL_KEY_ID_MAPPING_RESOURCE, idMappingResource);
@@ -74,7 +102,8 @@ public class LinnaeusAnnotator extends JCasAnnotator_ImplBase {
   public void initialize(UimaContext ctx) throws ResourceInitializationException {
     super.initialize(ctx);
     logger = ctx.getLogger();
-    logger.log(Level.CONFIG, "reading configuration file at " + configFilePath);
+    logger.log(Level.CONFIG, "reading configuration file for {0} annotations at {1}",
+        new String[] { namespace, configFilePath });
     ArgParser ap = new ArgParser(new String[] { "--properties", configFilePath });
     java.util.logging.Logger l = java.util.logging.Logger.getLogger("Linnaeus");
     l.setLevel(java.util.logging.Level.WARNING);
@@ -93,22 +122,22 @@ public class LinnaeusAnnotator extends JCasAnnotator_ImplBase {
         ++countIds;
         // Linnaeus sets p to NULL in some cases, so:
         if (probs[i] == null) probs[i] = 1.0 / ((double) probs.length);
-        if (idMapping != null && idMapping.containsKey(ids[i])) annotateSpecies(cas, mention,
+        if (idMapping != null && idMapping.containsKey(ids[i])) annotateMention(cas, mention,
             idMapping.get(ids[i]), probs[i]);
-        else annotateSpecies(cas, mention, ids[i], probs[i]);
+        else annotateMention(cas, mention, ids[i], probs[i]);
       }
     }
-    logger.log(Level.FINE, "tagged {0} mentions with {1} IDs", new Object[] { countMentions,
-        countIds });
+    logger.log(Level.FINE, "tagged {0} {1} mentions with {2} IDs", new Object[] { countMentions,
+        namespace, countIds });
   }
 
-  private void annotateSpecies(JCas cas, Mention mention, String id, Double prob) {
-    SemanticAnnotation species = new SemanticAnnotation(cas, mention.getStart(), mention.getEnd());
-    species.setAnnotator(URI);
-    species.setConfidence(prob);
-    species.setIdentifier(id);
-    species.setNamespace(NAMESPACE);
-    species.addToIndexes();
+  private void annotateMention(JCas cas, Mention mention, String id, Double prob) {
+    SemanticAnnotation ann = new SemanticAnnotation(cas, mention.getStart(), mention.getEnd());
+    ann.setAnnotator(URI);
+    ann.setConfidence(prob);
+    ann.setIdentifier(id);
+    ann.setNamespace(namespace);
+    ann.addToIndexes();
   }
 
   @Override
