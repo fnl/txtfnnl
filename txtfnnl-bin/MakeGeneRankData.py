144d23122f322fe1eea1c99dda68d2ee6d10f504
fnl <florian.leitner@gmail.com>
2013-06-25 14:49:54 +0200

    skeleton for gene ranking including sentence files

--- txtfnnl-bin/MakeGeneRankData.py
+++ txtfnnl-bin/MakeGeneRankData.py
@@ -10,8 +10,9 @@ from collections import namedtuple
 # 0:before, 1:prefix, 2:match, 3:suffix, 4:after, 5:pos, 6:offset, 7:gid, 8:name, 9:taxon, 10:entrez, 11:sim
 GENE_ITEMS = ["before", "prefix", "match", "suffix", "after", "pos", "offset", "gid", "name", "taxon", "entrez", "sim"]
 OFFSET = 6
-INTEGERS = [7, 9, 10]
+ENTREZ = 10
 KEYS = [8, 9, 10]
+INTEGERS = [7, 9, 10]
 Gene = namedtuple('Gene', GENE_ITEMS)
 
 def ParseGeneLines(filepath):
@@ -24,7 +25,14 @@ def ParseGeneLines(filepath):
             items[i] = txt[txt.find('"') + 1:txt.rfind('"')]
         for i in INTEGERS:
             items[i] = int(items[i])
-        yield Gene(items)
+        yield items[ENTREZ], Gene(items)
+
+def ParseSentenceLines(filepath):
+    # 0:match, 1:offset, 2:"Sentence" 3:conf
+    for line in open(filepath):
+        items = line.split('\t')
+        start, end = items[1].split(":")
+        yield (int(start), int(end)), float(items[3)
 
 def ParseTaxonLines(filepath):
     # 0:match, 1:offset, 2:tid 3:sim
@@ -38,7 +46,7 @@ def ParseGold(filepath):
         file_id, entrez = line.split('\t')
         yield file_id, int(entrez)
 
-def JoinData(genes, taxa, gold):
+def JoinData(genes, taxa, sentences, gold):
     pass
 
 def WriteLines(result_generator):
@@ -48,16 +56,18 @@ def WriteLines(result_generator):
             print u'\t%s' % ','.join(r).strip(),
         print
 
-def Process(gene_dir, taxon_dir, gold_file):
+def Process(gene_dir, taxon_dir, sentence_dir, gold_file):
     genes = {}
     taxa = {}
-    for filepath in os.listdir(gene_dir):
-        genes[filepath] = list(ParseGeneLines(filepath))
-    for filepath in os.listdir(taxon_dir):
-        taxa[filepath] = dict(ParseTaxonLines(filepath))
+    sentences = {}
     gold = dict(ParseGold(gold_file))
-    WriteData(JoinData(genes, taxa, gold))
+
+    for filepath in os.listdir(gene_dir):
+        genes = dict(ParseGeneLines(os.path.join(gene_dir, filepath))
+        taxa = dict(ParseTaxonLines(os.path.join(taxon_dir, filepath))
+        sentences = list(ParseTaxonLines(os.path.join(sentence_dir, filepath))
+        WriteData(JoinData(genes, taxa, sentences, gold))
 
 if __name__ == '__main__':
-    # gene_dir, taxon_dir, gold_file
+    # gene_dir, taxon_dir, sentence_dir, gold_file
     Process(*sys.argv[1:])
