1003d3b659545d4122e7c8b2f8c740d85ad6ec25
fnl <florian.leitner@gmail.com>
2013-06-25 12:52:40 +0200

    offset-based AnnotationLineWriter output for sentences

--- txtfnnl-bin/src/main/java/txtfnnl/pipelines/SentenceSplitter.java
+++ txtfnnl-bin/src/main/java/txtfnnl/pipelines/SentenceSplitter.java
@@ -1,34 +1,34 @@
 package txtfnnl.pipelines;
 
-import java.io.IOException;
-import java.util.logging.Logger;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
+import org.apache.commons.cli.*;
 import org.apache.uima.UIMAException;
-
+import txtfnnl.uima.analysis_component.opennlp.SentenceAnnotator;
+import txtfnnl.uima.collection.AnnotationLineWriter;
 import txtfnnl.uima.collection.OutputWriter;
 import txtfnnl.uima.collection.SentenceLineWriter;
 import txtfnnl.uima.collection.XmiWriter;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 /**
  * A plaintext sentence extractor pipeline for (nearly arbitrary) input files.
- * <p>
+ * <p/>
  * Input files can be read from a directory or listed explicitly, while output files are written to
  * a directory or to STDOUT. Output is always plain-text, where a single line contains (at most) a
  * single sentence.
- * 
+ *
  * @author Florian Leitner
  */
-public class SentenceSplitter {
-  private SentenceSplitter() {
+public
+class SentenceSplitter {
+  private
+  SentenceSplitter() {
     throw new AssertionError("n/a");
   }
 
-  public static void main(String[] arguments) {
+  public static
+  void main(String[] arguments) {
     final CommandLineParser parser = new PosixParser();
     CommandLine cmd = null;
     final Options opts = new Options();
@@ -38,25 +38,35 @@ public class SentenceSplitter {
     // sentence splitter options
     Pipeline.addSentenceAnnotatorOptions(opts);
     // output format options setup
-    opts.addOption("n", "maintain-newlines", false,
-        "do not replace newline chars within sentences with white-spaces");
+    opts.addOption(
+        "n", "maintain-newlines", false,
+        "do not replace newline chars within sentences with white-spaces"
+    );
     opts.addOption("a", "all-content", false, "include other (non-sentence) content in the output");
+    opts.addOption("O", "offsets", false, "write offsets of annotated sentences");
     try {
       cmd = parser.parse(opts, arguments);
     } catch (final ParseException e) {
       System.err.println(e.getLocalizedMessage());
       System.exit(1); // == EXIT ==
     }
-    final Logger l = Pipeline.loggingSetup(cmd, opts,
-        "txtfnnl split [options] <directory|files...>\n");
+    final Logger l = Pipeline.loggingSetup(
+        cmd, opts, "txtfnnl split [options] <directory|files...>\n"
+    );
     // output (format)
     OutputWriter.Builder writer;
     if (Pipeline.rawXmi(cmd)) {
-      writer = Pipeline.configureWriter(cmd,
-          XmiWriter.configure(Pipeline.ensureOutputDirectory(cmd)));
+      writer = Pipeline.configureWriter(
+          cmd, XmiWriter.configure(Pipeline.ensureOutputDirectory(cmd))
+      );
+    } else if (cmd.hasOption('O')) {
+      writer = Pipeline.configureWriter(cmd, AnnotationLineWriter.configure())
+                       .setAnnotatorUri(SentenceAnnotator.URI)
+                       .setAnnotationNamespace(SentenceAnnotator.NAMESPACE);
     } else {
-      SentenceLineWriter.Builder slwb = Pipeline.configureWriter(cmd,
-          SentenceLineWriter.configure());
+      SentenceLineWriter.Builder slwb = Pipeline.configureWriter(
+          cmd, SentenceLineWriter.configure()
+      );
       if (cmd.hasOption('n')) slwb.maintainNewlines();
       if (!cmd.hasOption('a')) slwb.excludeOtherContent();
       writer = slwb;
