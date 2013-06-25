d4c452ef9ae67ff35a9451fb2dde9a8f6ec12566
fnl <florian.leitner@gmail.com>
2013-06-25 15:37:10 +0200

    input parsing OK

--- txtfnnl-bin/MakeGeneRankData.py
+++ txtfnnl-bin/MakeGeneRankData.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Convert txtfnnl output from norm and match to rank data.
 """
@@ -7,32 +7,38 @@ import os
 import sys
 from collections import namedtuple
 
-# 0:before, 1:prefix, 2:match, 3:suffix, 4:after, 5:pos, 6:offset, 7:gid, 8:name, 9:taxon, 10:entrez, 11:sim
-GENE_ITEMS = ["before", "prefix", "match", "suffix", "after", "pos", "offset", "gid", "name", "taxon", "entrez", "sim"]
+# 0:before, 1:prefix, 2:match, 3:suffix, 4:after, 5:pos, 6:offset, 7:gid, 8:sim, 9:name, 10:taxon, 11:entrez
+GENE_ITEMS = ["before", "prefix", "match", "suffix", "after", "pos", "offset", "gid", "sim", "name", "taxon", "entrez"]
 OFFSET = 6
-ENTREZ = 10
-KEYS = [8, 9, 10]
-INTEGERS = [7, 9, 10]
+ENTREZ = 11
+KEYS = [9, 10, 11]
+INTEGERS = [7, 10, 11]
 Gene = namedtuple('Gene', GENE_ITEMS)
 
 def ParseGeneLines(filepath):
     for line in open(filepath):
         items = line.split('\t')
         start, end = items[OFFSET].split(":")
-        items[OFFSET] = tuple(int(start), int(end))
+        items[OFFSET] = (int(start), int(end))
         for i in KEYS:
             txt = items[i]
             items[i] = txt[txt.find('"') + 1:txt.rfind('"')]
         for i in INTEGERS:
-            items[i] = int(items[i])
-        yield items[ENTREZ], Gene(items)
+            try:
+                items[i] = int(items[i])
+            except ValueError:
+                print(filepath, file=sys.stderr)
+                print(line, file=sys.stderr)
+                print(items[i], i, file=sys.stderr)
+                raise
+        yield items[ENTREZ], Gene(*items)
 
 def ParseSentenceLines(filepath):
     # 0:match, 1:offset, 2:"Sentence" 3:conf
     for line in open(filepath):
         items = line.split('\t')
         start, end = items[1].split(":")
-        yield (int(start), int(end)), float(items[3)
+        yield (int(start), int(end)), float(items[3])
 
 def ParseTaxonLines(filepath):
     # 0:match, 1:offset, 2:tid 3:sim
@@ -50,11 +56,7 @@ def JoinData(genes, taxa, sentences, gold):
     pass
 
 def WriteLines(result_generator):
-    for tok, results in result_generator:
-        print '\t'.join(tok),
-        for r in results:
-            print u'\t%s' % ','.join(r).strip(),
-        print
+    pass
 
 def Process(gene_dir, taxon_dir, sentence_dir, gold_file):
     genes = {}
@@ -63,10 +65,10 @@ def Process(gene_dir, taxon_dir, sentence_dir, gold_file):
     gold = dict(ParseGold(gold_file))
 
     for filepath in os.listdir(gene_dir):
-        genes = dict(ParseGeneLines(os.path.join(gene_dir, filepath))
-        taxa = dict(ParseTaxonLines(os.path.join(taxon_dir, filepath))
-        sentences = list(ParseTaxonLines(os.path.join(sentence_dir, filepath))
-        WriteData(JoinData(genes, taxa, sentences, gold))
+        genes = dict(ParseGeneLines(os.path.join(gene_dir, filepath)))
+        taxa = dict(ParseTaxonLines(os.path.join(taxon_dir, filepath)))
+        sentences = list(ParseSentenceLines(os.path.join(sentence_dir, filepath)))
+        WriteLines(JoinData(genes, taxa, sentences, gold))
 
 if __name__ == '__main__':
     # gene_dir, taxon_dir, sentence_dir, gold_file
