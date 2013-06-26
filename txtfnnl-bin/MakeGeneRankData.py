ac5227d35a74c8b31ad6205234f2d138b5219bf6
fnl <florian.leitner@gmail.com>
2013-06-26 21:20:16 +0200

    inital results

--- txtfnnl-bin/MakeGeneRankData.py
+++ txtfnnl-bin/MakeGeneRankData.py
@@ -11,7 +11,7 @@ from collections import namedtuple
 GENE_ITEMS = ["before", "prefix", "match", "suffix", "after", "pos", "offset", "gid", "sim", "name", "taxon", "entrez"]
 OFFSET = 6
 MENTION = 2
-GID = 11
+GID = 7
 KEYS = [9, 10, 11]
 INTEGERS = [7, 10, 11]
 Gene = namedtuple('Gene', GENE_ITEMS)
@@ -67,7 +67,7 @@ def ParseCounters(filepath):
             raise
         yield int(gid), sym, int(refc), int(globc)
 
-def JoinData(genes, taxa, sentences, gold, links, symbols, references):
+def JoinData(count, genes, taxa, sentences, gold, links, symbols, references):
     sym_counts = {}
     for gid, data in genes.items():
         for sym , mentions in data.items():
@@ -90,12 +90,27 @@ def JoinData(genes, taxa, sentences, gold, links, symbols, references):
         count_GID = sum(len(m) for m in data.values())
 
         for sym, mentions in data.items():
+            name = mentions[0].name
             count_SYM = sym_counts[sym]
             count_GIDSYM = len(mentions)
             count_links = links[gid] if gid in links else 0
-            count_sym = symbols[sym]
-            count_refs = references[gid][sym]
-            count_tids = len(taxa[mentions.taxon])
+            try:
+                count_sym = symbols[name]
+            except KeyError:
+                print('unknown name "{}" in mention "{}"'.format(name, gid))
+                continue
+            try:
+                count_refs = references[gid][name]
+            except KeyError:
+                print('unknown gid "{}" and name "{}"'.format(gid, name))
+                continue
+            count_tids = 0
+            if taxa:
+                try:
+                    count_tids = len(taxa[mentions[0].taxon])
+                except KeyError:
+                    print('unknown taxon "{}" in mention "{}"'.format(mentions[0].taxon, gid), file=sys.stderr)
+                    continue
             tid_in_sent = 0
             if mentions[0].taxon in taxa_sentences:
                 sent_offsets = taxa_sentences[mentions[0].taxon]
@@ -103,15 +118,28 @@ def JoinData(genes, taxa, sentences, gold, links, symbols, references):
                     if any(m.offset[0] >= s[0] and m.offset[1] <= s[1] for s in sent_offsets):
                         tid_in_sent = 1
                         break
-            yield gid in gold, counter(), (count_GID, count_SYM, count_GIDSYM, count_links, count_sym, count_refs, count_tids, tid_in_sent)
+
+            yield mentions[0].entrez in gold, count, (mentions[0].sim, count_GID, count_SYM, count_GIDSYM, count_links, count_sym, count_refs, count_tids, tid_in_sent)
 
 def WriteLines(result_generator):
     for is_hit, qid, features in result_generator:
-        print(int(is_hit), 'qid:{}'.format(qid), ' '.join('{}:{}'.format(i+1, f) for f in enumerate(features)))
+        print(int(is_hit), 'qid:{}'.format(qid), ' '.join('{}:{}'.format(i+1, f) for i, f in enumerate(features)))
 
 def Process(gene_dir, taxon_dir, sentence_dir, gold_file, counter_file, linkoutcount_file):
-    gold = dict(ParseGold(gold_file))
+    gold = {}
+
+    for article_id, entrez_id in ParseGold(gold_file):
+        if article_id in gold:
+            gold[article_id].add(entrez_id)
+        else:
+            gold[article_id] = {entrez_id}
+
+    print("parsed {} gold items".format(len(gold)), file=sys.stderr)
+
     link_counts = dict(ParseLinkoutCount(linkoutcount_file))
+
+    print("parsed {} link items".format(len(link_counts)), file=sys.stderr)
+
     sym_counts = {}
     ref_counts = {}
 
@@ -123,12 +151,17 @@ def Process(gene_dir, taxon_dir, sentence_dir, gold_file, counter_file, linkoutc
         else:
             ref_counts[gid][sym] = refc
 
+    print("parsed {} refcount items".format(len(ref_counts)), file=sys.stderr)
+    count = 0
+
     for filepath in os.listdir(gene_dir):
         article_id = filepath[:filepath.rfind('.')]
 
         if article_id not in gold:
+            print("skipping", article_id, file=sys.stderr)
             continue
 
+        print("processing", article_id, file=sys.stderr)
         genes = {}
         taxa = {}
 
@@ -143,12 +176,13 @@ def Process(gene_dir, taxon_dir, sentence_dir, gold_file, counter_file, linkoutc
 
         for tid, offset in ParseTaxonLines(os.path.join(taxon_dir, filepath)):
             if tid in taxa:
-                tid.add(offset)
+                taxa[tid].add(offset)
             else:
                 taxa[tid] = {offset}
 
         sentences = list(ParseSentenceLines(os.path.join(sentence_dir, filepath)))
-        WriteLines(JoinData(genes, taxa, sentences, gold[article_id], link_counts, sym_counts, ref_counts))
+        count += 1
+        WriteLines(JoinData(count, genes, taxa, sentences, gold[article_id], link_counts, sym_counts, ref_counts))
 
 if __name__ == '__main__':
     # gene_dir, taxon_dir, sentence_dir, gold_file, counter_file, refcount_file
